fd5c0a5084af35dd9fb7285079a2e0224057d004
Nikolay <h09boi@gmail.com>
2023-02-19 15:29:16 +0300

    fix script

--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,16 @@ const soundToggle = document.querySelector('.audio__on-icon');
 const audioControls = document.querySelector('.audio-controls');
 const soundOffToggle = document.querySelector('.audio__off');
 
+window.onload = function () {
+  document.getElementById("my_audio").play();
+}
+
 soundToggle.addEventListener('click', function () {
-  audioControls.muted = true;
-  soundOffToggle.classList.remove('visually-hidden');
+  if (soundOffToggle.classList.contains('visually-hidden')) {
+    soundOffToggle.classList.remove('visually-hidden');
+    audioControls.muted = true;
+  } else {
+    soundOffToggle.classList.add('visually-hidden');
+    audioControls.muted = false;
+  }
 });
